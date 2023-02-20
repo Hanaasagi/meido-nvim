f4cfc738c3b795478ca2c4e0768d8d99e3ce8b58
Hanaasagi <ambiguous404@gmail.com>
2023-02-20 13:47:29 +0800

    feat(plugin): ignore empty line in comment.nvim

--- lua/plugins/operation.lua
+++ lua/plugins/operation.lua
@@ -46,6 +46,7 @@ return {
     config = function()
       require('Comment').setup({
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        ignore = '^$',
         toggler = {
           -- Line-comment toggle keymap
           line = '<leader>cc',
