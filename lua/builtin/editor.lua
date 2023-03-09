851d331aef9ea71963352f10a634e47b14151909
Hanaasagi <ambiguous404@gmail.com>
2023-03-09 14:53:46 +0800

    feat: hide cursorline when window is inactive

--- lua/builtin/editor.lua
+++ lua/builtin/editor.lua
@@ -111,3 +111,32 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 
+local function setupDimCursorLine()
+  local old_guicursor, old_cursorline, old_cursorcolumn
+
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      old_guicursor = vim.opt.guicursor
+      old_cursorline = vim.opt.cursorline
+      old_cursorcolumn = vim.opt.cursorcolumn
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "WinLeave", "FocusLost" }, {
+    callback = function()
+      vim.opt.guicursor = "a:noCursor"
+      vim.opt.cursorline = false
+      vim.opt.cursorcolumn = false
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "WinEnter", "FocusGained" }, {
+    callback = function()
+      vim.opt.guicursor = old_guicursor
+      vim.opt.cursorline = old_cursorline
+      vim.opt.cursorcolumn = old_cursorcolumn
+    end,
+  })
+end
+
+setupDimCursorLine()
