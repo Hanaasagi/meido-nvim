d4b063b2cc9974f33291add6ad6ac656e8685cc8
Hanaasagi <ambiguous404@gmail.com>
2023-03-10 11:21:19 +0800

    feat: add treesitter playground

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -132,6 +132,34 @@ return {
     end,
   },
 
+  -- https://github.com/nvim-treesitter/playground
+  -- View treesitter information directly in Neovim!
+  {
+    "nvim-treesitter/playground",
+    config = function()
+      require"nvim-treesitter.configs".setup {
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+          persist_queries = false, -- Whether the query persists across vim sessions
+          keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+          },
+        },
+      }
+    end,
+  },
+
   {
     "folke/trouble.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
