4aa162eeed91a5b2089fdebbc5994f7716f382f9
Hanaasagi <ambiguous404@gmail.com>
2023-03-10 11:24:41 +0800

    feat: add trim.nvim

--- lua/plugins/operation.lua
+++ lua/plugins/operation.lua
@@ -200,4 +200,28 @@ return {
     end,
 
   },
+
+  -- https://github.com/cappyzawa/trim.nvim
+  -- This plugin trims trailing whitespace and lines.
+  {
+    "cappyzawa/trim.nvim",
+    config = function()
+      require('trim').setup({
+        -- if you want to ignore markdown file.
+        -- you can specify filetypes.
+        ft_blocklist = { "markdown" },
+
+        -- if you want to remove multiple blank lines
+        patterns = {
+          [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+        },
+
+        -- if you want to disable trim on write by default
+        trim_on_write = true,
+        trim_trailing = true,
+        trim_last_line = true,
+        trim_first_line = true,
+      })
+    end,
+  },
 }
