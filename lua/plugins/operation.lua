6fb7af834fc20fa88c6ed2444612d1a8f8e4d959
Hanaasagi <ambiguous404@gmail.com>
2023-03-10 11:25:21 +0800

    feat: disable current line blame virtual text

--- lua/plugins/operation.lua
+++ lua/plugins/operation.lua
@@ -146,7 +146,7 @@ return {
       gitsigns.setup({
         current_line_blame = true,
         current_line_blame_opts = {
-          virt_text = true,
+          virt_text = false,
           virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
           delay = 100,
           ignore_whitespace = false,
