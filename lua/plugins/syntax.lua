20d764c37516fefb9149b342aaae435c20224bac
Hanaasagi <ambiguous404@gmail.com>
2023-03-30 21:45:23 +0800

    fix: fix wrong python indent

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -127,6 +127,7 @@ return {
           -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
         },
+        indent = { enable = true },
       }
 
     end,
