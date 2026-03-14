dc0a4e2bbe6480161d4e553c52522c660cfbfe0c
Hanaasagi <ambiguous404@gmail.com>
2026-03-14 18:03:36 +0900

    feat: gitlink

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -178,4 +178,14 @@ return {
       require("which-key").setup({})
     end,
   },
+
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      -- { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      -- { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
 }
